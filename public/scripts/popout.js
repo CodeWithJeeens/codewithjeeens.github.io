4df74faeb4440d522c25df421be3de4ad1eb7287
CodeWithJeeens <69648341+CodeWithJeeens@users.noreply.github.com>
2023-04-12 21:04:20 +0200

    test

--- public/scripts/popout.js
+++ public/scripts/popout.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-  // Elemente abrufen
-  var overlay = $('.overlay');
-  var popup = $('.popup');
-  var closeBtn = $('.close-btn');
-  var openBtn = $('#open-btn');
+    // Elemente abrufen
+    var overlay = $('.overlay');
+    var popup = $('.popup');
+    var closeBtn = $('.close-btn');
+    var openBtn = $('#open-btn');
 
-  // Klick-Event-Listener hinzufügen
-  openBtn.click(function() {
-    overlay.fadeIn();
-    popup.addClass('show');
-  });
+    // Klick-Event-Listener hinzufügen
+    openBtn.click(function() {
+        overlay.fadeIn();
+        popup.addClass('show');
+    });
 
-  closeBtn.click(function() {
-    overlay.fadeOut();
-    popup.removeClass('show');
-  });
+    closeBtn.click(function() {
+        overlay.fadeOut();
+        popup.removeClass('show');
+    });
 });
